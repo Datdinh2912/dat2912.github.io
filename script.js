43cf8d999ae02160bf9d6ee22f8ede7d8a2f3d8d
dat2912 <68189463+dat2912@users.noreply.github.com>
2021-03-14 21:45:05 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
-function myFunction() {
+function mode() {
    var element = document.body;
    element.classList.toggle("dark-mode");
+}
+function more() {
+   var element = document.body;
+   element.classList.toggle("smalltab");
 }
